a4d19066dd344820830a6a372c7a657f066e658f
Bartłomiej Zglinicki <78939671+bzglinicki@users.noreply.github.com>
2022-03-01 13:59:39 +0100

    Usuwanie starych wersji notatek

--- Notatki/1_Podstawy/1_Skladnia-podstawowe-instrukcje/1_Pierwszy-program.py
+++
@@ -1,5 +0,0 @@
-# Programowanie I R
-# Pierwszy program
-
-name = input("Jak masz na imię? ")
-print("Witaj,", name)
--- Notatki/6_Testy-jednostkowe/1_Przyklad-1/mul_calc.py
+++
@@ -1,8 +0,0 @@
-def mul(a, b):
-    if type(a) not in [int, float]:
-        raise TypeError("Pierwszy czynnik musi być liczbą!")
-    
-    if type(b) not in [int, float]:
-        raise TypeError("Drugi czynnik musi być liczbą!")
-    
-    return a * b
